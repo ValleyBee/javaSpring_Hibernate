d97267b3a5ef826beab014793b89044068e155b6
VallenBee <csplus2010.vb@gmail.com>
2022-07-28 09:12:38 +0300

    add Aspects

--- src/main/java/aop/aspects/LoggingAspect.java
+++ src/main/java/aop/aspects/LoggingAspect.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class LoggingAspect {
+    public void beforeGetBookAdvice(){
+        System.out.println("beforeGetBookAdvice: attempt to get a book");
+    }
 }
