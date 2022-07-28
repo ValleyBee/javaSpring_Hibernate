cd7641e4416487be02a7ffd8c9f435bb6438217b
VallenBee <csplus2010.vb@gmail.com>
2022-07-28 08:45:56 +0300

    add classes

--- src/main/java/aop/Library.java
+++ src/main/java/aop/Library.java
@@ -1,5 +1,11 @@
 package aop;
 
+import org.springframework.stereotype.Component;
+
+@Component("libraryBean")
 public class Library {
 
+    public void getBook(){
+        System.out.println("we are taking a book");
+    }
 }
