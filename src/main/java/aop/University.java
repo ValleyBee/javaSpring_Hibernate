0e14f680c264ca266e3fa9dfcb1e10cd5ed1c293
VallenBee <csplus2010.vb@gmail.com>
2022-07-29 13:56:17 +0300

    add Class

---
+++ src/main/java/aop/University.java
@@ -0,0 +1,30 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class University {
+    private List<Student> studentList = new ArrayList<>();
+
+
+    public void addStudent(){
+Student st1 = new Student("MarkBer",1,100.00);
+Student st2 = new Student("Jacub",1,99.99);
+Student st3 = new Student("Julia",8,300.00);
+studentList.add(st1);
+studentList.add(st2);
+studentList.add(st3);
+    }
+
+    public List<Student> getStudentList (){
+
+        System.out.println("information from method getStudents");
+        System.out.println(studentList);
+        return studentList;
+
+    }
+
+}
---
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -0,0 +1,8 @@
+package aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class UniversityLoggingAspect {
+
+}
