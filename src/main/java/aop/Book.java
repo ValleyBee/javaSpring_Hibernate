1364789bd14ad9ef473751d7685fce8be45606c3
VallenBee <csplus2010.vb@gmail.com>
2022-07-29 10:50:01 +0300

    add Join Point

--- src/main/java/aop/Book.java
+++ src/main/java/aop/Book.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Book{
-    @Value("Spring join point") // this replaces setters
+    @Value("Spring join point") // this replaces setters. It will set an arg during creating a container bean.
     private String name;
     @Value("ZT")
     private String author;
