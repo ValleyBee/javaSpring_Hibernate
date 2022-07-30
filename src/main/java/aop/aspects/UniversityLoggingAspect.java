d2a0dc910b986dff13f72e9bbb9b3a54d06614a3
VallenBee <csplus2010.vb@gmail.com>
2022-07-30 10:56:19 +0300

    add catch throw advice

--- src/main/java/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -1,10 +1,8 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,26 +10,34 @@ import java.util.List;
 @Component
 @Aspect
 public class UniversityLoggingAspect {
-@Before("execution(* getStudentList())")
-    public void beforeGetStudentLoggingAdvice(){
-    System.out.println("beforeGetStudentLoggingAdvice: Logging get " + " before method Students List");
+    @Before("execution(* getStudentList())")
+    public void beforeGetStudentLoggingAdvice() {
+        System.out.println("beforeGetStudentLoggingAdvice: Logging get " + " before method Students List");
+        System.out.println("---------------------------------------------------");
     }
 
-    @AfterReturning(pointcut = "execution(* getStudentList())",returning = "listStudents")
-    public void afterReturnGetStudentLoggingAdvice(List<Student> listStudents){
-    Student firstStudent = listStudents.get(0);
-    String name = firstStudent.getNameSurname();
-    name = "Mr." + name;
-    firstStudent.setNameSurname(name);
-
-
+    @AfterReturning(pointcut = "execution(* getStudentList())", returning = "listStudents")
+    public void afterReturnGetStudentLoggingAdvice(List<Student> listStudents) {
+        Student firstStudent = listStudents.get(0);
+        String name = firstStudent.getNameSurname();
+        name = "Mr." + name;
+        firstStudent.setNameSurname(name);
         System.out.println("afterReturnGetStudentLoggingAdvice: Logging get " + " after method Students List");
+        System.out.println("---------------------------------------------------");
     }
 
-    @AfterThrowing(pointcut = "execution(* getStudentList())",throwing = "exception")
-    public void afterThrowingGetStudentLoggingAdvice(Throwable exception){
+    @AfterThrowing(pointcut = "execution(* getStudentList())", throwing = "exception")
+    public void afterThrowingGetStudentLoggingAdvice(Throwable exception) {
         System.out.println("afterThrowingGetStudentLoggingAdvice: Logging throwing = " + exception);
+        System.out.println("---------------------------------------------------");
 
+    }
+
+    @After("execution(* getStudentList())")
+    public void afterGetStudentLoggingAdvice(JoinPoint joinPoint) {
+        System.out.println("afterGetStudentLoggingAdvice : " + " Logging normal or throw ending of code");
+        System.out.println("joinPoint getSignature : " + joinPoint.getSignature());
+        System.out.println("---------------------------------------------------");
     }
 
 }
