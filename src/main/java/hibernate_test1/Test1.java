a60cd6a199ea3f878e0728dc523b25cb492541a7
VallenBee <csplus2010.vb@gmail.com>
2022-08-02 13:26:56 +0300

    INSERT SELECT one session

--- src/main/java/hibernate_test1/Test1.java
+++ src/main/java/hibernate_test1/Test1.java
@@ -1,3 +1,6 @@
+/* Creating a session
+add to database
+*  */
 package hibernate_test1;
 
 
@@ -7,21 +10,22 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class Test1 {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Employee.class).buildSessionFactory();
-        try {
-            Session session = factory.getCurrentSession();
-            Employee emp = new Employee("Val", "Bersh", "Owner", 4000);
-            session.beginTransaction();
-            session.save(emp); //INSERT SQL
-            session.getTransaction().commit();
-            System.out.println("operation with database done");
-        } finally {
-            factory.close();
-        }
+       SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Employee.class).buildSessionFactory();
+       try {
+           Session session = factory.getCurrentSession();
+           Employee emp = new Employee("David", "Valen", "Sales", 3500);
+           session.beginTransaction();
+           session.save(emp); //INSERT SQL
+           session.getTransaction().commit();
+           System.out.println("operation with database done");
+           System.out.println(emp);
+       } finally {
+           factory.close();
+       }
 
 
-    }
+   }
 
 }
---
+++ src/main/java/hibernate_test1/Test2.java
@@ -0,0 +1,41 @@
+ /* add to database and  get from database in one session  */
+package hibernate_test1;
+
+
+import hibernate_test1.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Employee.class).buildSessionFactory();
+        try {
+            /* add to database */
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Abel", "Adam", "Sales", 2550);
+            session.beginTransaction();
+            /* save is INSERT SQL */
+            session.save(emp);
+
+            /* get from database */
+            int myId = emp.getId();
+
+/* get from table by SELECT and convert data to Object Employee
+ get is SELECT SQL */
+            Employee employee = session.get(Employee.class, myId);
+            session.getTransaction().commit();
+            System.out.println(employee);
+
+
+            System.out.println("operation with database done");
+
+        } finally {
+            factory.close();
+        }
+
+
+    }
+
+}
