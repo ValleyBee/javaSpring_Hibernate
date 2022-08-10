8f3116a12ca8da1e9eb0a89b061840ca4a4ff747
VallenBee <csplus2010.vb@gmail.com>
2022-08-10 10:03:45 +0300

    @OneToMany

--- src/main/java/hibernate_one_to_many_bi/entity/Department.java
+++ src/main/java/hibernate_one_to_many_bi/entity/Department.java
@@ -1,6 +1,8 @@
 package hibernate_one_to_many_bi.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="departments")
@@ -16,6 +18,10 @@ public class Department {
     @Column(name = "min_salary")
     private int minSalary;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "department")
+    private List<Employee> listEmps;
+
+
     public Department() {
     }
 
@@ -24,5 +30,61 @@ public class Department {
         this.department_name = department_name;
         this.maxSalary = maxSalary;
         this.minSalary = minSalary;
+    }
+public void addEmpsToDepartment(Employee employee){
+        if( listEmps == null) {
+            listEmps = new ArrayList<>();
+        }
+        listEmps.add(employee);
+        employee.setDepartment(this);
+}
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartment_name() {
+        return department_name;
+    }
+
+    public void setDepartment_name(String department_name) {
+        this.department_name = department_name;
+    }
+
+    public int getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(int maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public int getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(int minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public List<Employee> getEmps() {
+        return listEmps;
+    }
+
+    public void setEmps(List<Employee> emps) {
+        this.listEmps = emps;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", department_name='" + department_name + '\'' +
+                ", maxSalary=" + maxSalary +
+                ", minSalary=" + minSalary +
+                '}';
     }
 }
--- src/main/java/hibernate_one_to_many_bi/entity/Employee.java
+++ src/main/java/hibernate_one_to_many_bi/entity/Employee.java
@@ -41,5 +41,53 @@ public class Employee {
     private Department department;
 
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
