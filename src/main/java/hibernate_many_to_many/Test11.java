1db3c7dbd643ebd9b169938c30230bf1bc31f4f1
VallenBee <csplus2010.vb@gmail.com>
2022-08-11 08:51:35 +0300

    ManyToMany

--- src/main/java/hibernate_many_to_many/Test11.java
+++ src/main/java/hibernate_many_to_many/Test11.java
@@ -6,9 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test {
+public class Test11 {
     public static void main(String[] args) {
-
         SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Child.class)
                 .addAnnotatedClass(Section.class)
@@ -19,34 +18,11 @@ public class Test {
             session = factory.getCurrentSession();
             session.beginTransaction();
 
-//Section sec1 = new Section("football");
-//Section sec2 = new Section("baseball");
-Section sec1 = session.get(Section.class,1); // add existing
-Section sec2 = session.get(Section.class,2); // add existing
-Section sec3 = new Section("hockey");
+            Child child = session.get(Child.class,4);
 
-//Child child1 = new Child("Mark",10);
-//Child child2 = new Child("Yakiv",5);
-//Child child3 = new Child("Julia",12);
+            session.delete(child);
 
-
-Child child4 = new Child("Kote",3);
-child4.addChildSection(sec1);
-child4.addChildSection(sec2);
-child4.addChildSection(sec3);
-session.save(child4);
-
-//sec1.addChildSection(child1);
-//sec1.addChildSection(child2);
-//sec2.addChildSection(child1);
-//sec2.addChildSection(child3);
-
-//session.save(sec1);
-//session.save(sec2);
-
-
-session.getTransaction().commit();
-
+            session.getTransaction().commit();
 
         } finally {
             assert session != null;
@@ -55,8 +31,5 @@ session.getTransaction().commit();
             factory.close();
             System.out.println("factory close");
         }
-
     }
-
-
 }
